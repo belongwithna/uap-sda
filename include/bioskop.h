572d50e3750be1a58a1d328619cd72ed3b6875f8
Naura <nauraalifia449@gmail.com>
2026-06-09 15:08:32 +0700

    update kode bioskop.h

--- include/bioskop.h
+++ include/bioskop.h
@@ -2,32 +2,25 @@
 #define BIOSKOP_H
 
 #include "film.h"
-#include "kursi.h"
 #include "queue.h"
 #include "stack.h"
 
 class Bioskop {
 
 private:
-
     FilmList* filmList;
-
-    Kursi* kursi;
-
     TicketQueue* queue;
-
     HistoryStack* history;
 
 public:
-
     Bioskop(
         FilmList* film,
-        Kursi* kursiObj,
         TicketQueue* q,
         HistoryStack* h
     );
 
     void pesanTiket();
+    void lihatKursiFilm();
 };
 
 #endif
